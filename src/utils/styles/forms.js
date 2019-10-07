1cfae7537655019106a84531272a324f54558c57
Heidi Mason <hapaheidi@gmail.com>
2019-10-07 13:33:17 -0700

    style: Revise form styles

--- src/utils/styles/forms.js
+++ src/utils/styles/forms.js
@@ -1,16 +1,16 @@
 import { StyleSheet } from 'react-native'
-import { grayLight, grayXLight } from './colors'
+import { grayLight, grayXLight, white } from './colors'
 
 export const forms = StyleSheet.create({
-	iosTextInput: {
-		borderRadius: 7
-	},
     textInput: {
-    	backgroundColor: grayLight,
+    	backgroundColor: white,
         color: grayXLight,
         height: 45,
         marginHorizontal: 40,
         padding: 10,
         textAlign: 'center'
+    },
+    textInputIOS: {
+        borderRadius: 7
     }
 })
