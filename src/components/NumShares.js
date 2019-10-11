7d93b1673b3b596d5cbfed0c7e54f067bfde8ddc
Heidi Mason <hapaheidi@gmail.com>
2019-10-11 16:00:38 -0700

    feat: Add validations cont.; refactor: TextInput components -> stateless func components

--- src/components/NumShares.js
+++ src/components/NumShares.js
@@ -1,39 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Platform, TextInput, View } from 'react-native'
 import { grayDark } from '../utils/styles/colors'
 import { fonts } from '../utils/styles/fonts'
 import { forms } from '../utils/styles/forms'
 import styled from 'styled-components/native'
 
-class NumShares extends Component {
+const NumShares = props => {
+	const { shareNum } = props
+
 	handleNumChange = num => {
-		const { onNumChange } = this.props
+		const { onNumChange } = props
 
 		onNumChange(num)
 	}
 
-	render() {
-		const { shareNum } = this.props
-
-		return (
-			<NumSharesInput
-		        keyboardType="numeric"
-		        onChangeText={this.handleNumChange}
-				placeholder="Number of Shares"
-				selectionColor={grayDark}
-	        	style={[
-	        		Platform.OS === 'ios'
-	        		? forms.inputIOS
-	        		: forms.inputAndroid,
-	        		shareNum !== '' && shareNum <= 0
-	        		? forms.textInputError
-	        		: forms.textInputValid,
-					[fonts.h2, forms.textInput]
-				]}
-				value={shareNum.toString()}
-	        />
-		)
-	}
+	return (
+		<NumSharesInput
+	        keyboardType="numeric"
+	        onChangeText={this.handleNumChange}
+			placeholder="Number of Shares"
+			selectionColor={grayDark}
+        	style={[
+        		Platform.OS === 'ios'
+        		? forms.inputIOS
+        		: forms.inputAndroid,
+        		shareNum !== '' && shareNum <= 0
+        		? forms.textInputError
+        		: forms.textInputValid,
+				[fonts.h2, forms.textInput]
+			]}
+			value={shareNum.toString()}
+        />
+	)
 }
 
 const NumSharesInput = styled.TextInput`
--- src/components/PriceOfShares.js
+++ src/components/PriceOfShares.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Platform, TextInput, View } from 'react-native'
 import { grayDark } from '../utils/styles/colors'
 import { fonts } from '../utils/styles/fonts'
 import { forms } from '../utils/styles/forms'
 
-class PriceOfShares extends Component {
+const PriceOfShares = props => {
+	const { sharePrice } = props
+
 	handlePriceChange = price => {
-		const { onPriceChange } = this.props
+		const { onPriceChange } = props
 
 		onPriceChange(price)
 	}
 
-	render() {
-		const { sharePrice } = this.props
-
-		return (
-			<TextInput
-		        keyboardType="numeric"
-				onChangeText={this.handlePriceChange}
-				placeholder="Price per Share ($)"
-				selectionColor={grayDark}
-	        	style={[
-	        		Platform.OS === 'ios'
-	        		? forms.inputIOS
-	        		: forms.inputAndroid,
-					[fonts.h2, forms.textInput]
-				]}
-				value={sharePrice}
-	        />
-		)
-	}
+	return (
+		<TextInput
+	        keyboardType="numeric"
+			onChangeText={this.handlePriceChange}
+			placeholder="Price per Share ($)"
+			selectionColor={grayDark}
+        	style={[
+        		Platform.OS === 'ios'
+        		? forms.inputIOS
+        		: forms.inputAndroid,
+        		sharePrice !== '' && sharePrice <= 0
+        		? forms.textInputError
+        		: forms.textInputValid,
+				[fonts.h2, forms.textInput]
+			]}
+			value={sharePrice}
+        />
+	)
 }
 
 export default PriceOfShares
