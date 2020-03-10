f65da1d5b08996b8e25d5521712d530944e92e14
Heidi Mason <hapaheidi@gmail.com>
2020-03-10 05:04:26 -0700

    fix: Specify which position (ie front or back) payload is added to state

--- src/reducers/PurchaseHistory.js
+++ src/reducers/PurchaseHistory.js
@@ -7,7 +7,20 @@ import {
 const purchaseHistory = (state = [], action) => {
 	switch (action.type) {
 		case ADD_PURCHASE:
-			return state.concat(action.payload)
+			if (state === []) {
+				// Add payload object to end of array
+				return state.concat(action.payload)
+			} else {
+				if (state[0] === undefined || state[0].index === 1) {
+					// Add payload object to end of array
+					return state.concat(action.payload)
+				} else {
+				 	// Add payload object to beginning of array
+					state.unshift(action.payload)
+
+					return state
+				}
+			}
 		case CLEAR_PURCHASES:
 			return []
 		case REVERSE_PURCHASES:
--- src/reducers/SaleHistory.js
+++ src/reducers/SaleHistory.js
@@ -7,7 +7,20 @@ import {
 const saleHistory = (state = [], action) => {
 	switch (action.type) {
 		case ADD_SALE:
-			return state.concat(action.payload)
+			if (state === []) {
+				// Add payload object to end of array
+				return state.concat(action.payload)
+			} else {
+				if (state[0] === undefined || state[0].index === 1) {
+					// Add payload object to end of array
+					return state.concat(action.payload)
+				} else {
+				 	// Add payload object to beginning of array
+					state.unshift(action.payload)
+
+					return state
+				}
+			}
 		case CLEAR_SALES:
 			return []
 		case REVERSE_SALES:
