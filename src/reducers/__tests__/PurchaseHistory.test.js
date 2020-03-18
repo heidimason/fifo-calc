0296bd63adef00e8e8f403f2a2231575f58ae243
Heidi Mason <hapaheidi@gmail.com>
2020-03-18 09:08:04 -0700

    test: Add Purchase History Reducer tests

--- src/reducers/__tests__/PurchaseHistory.test.js
+++ src/reducers/__tests__/PurchaseHistory.test.js
@@ -1,17 +1,79 @@
-import purchaseHistory from '../PurchaseHistory'
+import purchaseHistoryReducer from '../PurchaseHistory'
 import {
 	ADD_PURCHASE,
+	CLEAR_PURCHASES,
 	REVERSE_PURCHASES
 } from '../../actions/PurchaseHistory'
 
-it('handles actions of type ADD_PURCHASE', () => {
-	const action = {
-		type: ADD_PURCHASE,
-		payload: 'New Purchase'
-	},
+describe('Purchase History Reducer', () => {
+	it('returns the initial state', () => {
+		const expected = []
 
-	newState = purchaseHistory([], action)
+		expect( purchaseHistoryReducer(undefined, []) ).toEqual(expected)
+	})
 
-	expect(newState).toEqual(['New Purchase'])
+	it('handles actions of type ADD_PURCHASE', () => {
+		const action = {
+			type: ADD_PURCHASE,
+			payload: 'New Purchase'
+		},
+
+		expected = ['New Purchase'],
+
+		newState = purchaseHistoryReducer([], action)
+
+		expect(newState).toEqual(expected)
+	})
+
+	it('handles actions of type CLEAR_PURCHASES', () => {
+		const action = {
+			type: CLEAR_PURCHASES
+		},
+
+		expected = [],
+
+		initialState = {
+			index: 1,
+			num: 100,
+			price: 25
+		}
+
+		expect( purchaseHistoryReducer(initialState, action) ).toEqual(expected)
+	})
+
+	it('handles actions of type REVERSE_PURCHASES', () => {
+		const action = {
+			type: REVERSE_PURCHASES
+		},
+
+		expected = [
+			{
+				index: 2,
+				num: 100,
+				price: 30
+			},
+			{
+				index: 1,
+				num: 100,
+				price: 25
+			}
+		],
+
+		initialState = [
+			{
+				index: 1,
+				num: 100,
+				price: 25
+			},
+			{
+				index: 2,
+				num: 100,
+				price: 30
+			}
+		]
+
+		expect( purchaseHistoryReducer(initialState, action) ).toEqual(expected)
+	})
 })
+
 
